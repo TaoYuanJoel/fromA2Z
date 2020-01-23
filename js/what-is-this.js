6170c5412e70bee2097ebe9a463547a547686ec6
TaoYuanJoel <ytao03@risd.edu>
2020-01-22 22:50:49 -0500

    in-class

---
+++ js/what-is-this.js
@@ -0,0 +1,7 @@
+"use strict";
+while (true) {
+    let year = prompt('In which year was ECMAScript-2015 specification published?', '');
+
+    year == 2015 ? alert("right") : alert("wrong");
+    year == 2015 ? break; 
+}
